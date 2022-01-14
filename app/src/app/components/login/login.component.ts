79c8149e3b739fc048c04efb564aea89b52bf379
Christian Makoma <christianmakoma@Paulines-MacBook-Pro.local>
2022-01-13 20:38:27 -0700

    added user service

--- app/src/app/components/login/login.component.ts
+++ app/src/app/components/login/login.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl } from '@angular/forms';
+import { Observable } from 'rxjs';
+import { User } from '../../../models/user';
+import { UserService } from '../../../services/user.service';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +11,31 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
-
-  ngOnInit(): void {
+  loading$: Observable<boolean>;
+  users$: Observable<User[]>;
+  user$: Observable<User>;
+ 
+  constructor(private userService: UserService) {
+    this.users$ = userService.entities$;
+    this.loading$ = userService.loading$;
+    this.user$ = userService.;
+    
+  }
+ 
+  ngOnInit() {
+    this.getuseres();
+  }
+ 
+  add(user: User) {
+    this.userService.add(user);
+  }
+ 
+  getuseres() {
+    this.userService.getAll();
+  }
+ 
+  update(user: User) {
+    this.userService.update(user);
   }
 
 }
