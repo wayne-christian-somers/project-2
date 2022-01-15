450af227620eb93d459fa33397429661f9a77818
Somers B Matthews <somersbmatthews@gmail.com>
2022-01-14 21:36:36 -0600

    about to run new container generator

--- app/src/app/app.module.ts
+++ app/src/app/app.module.ts
@@ -3,28 +3,32 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { LoginComponent } from './components/login/login.component';
 import { NavComponent } from './components/nav/nav.component';
 import { HttpClientModule } from '@angular/common/http';
 import { EffectsModule } from '@ngrx/effects';
 import { StoreModule } from '@ngrx/store';
 import { StoreDevtoolsModule } from '@ngrx/store-devtools';
 import { environment } from '../environments/environment';
+import { reducers, metaReducers } from './store';
+
+
+
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent,
     NavComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
-    StoreModule.forRoot({}),
-    EffectsModule.forRoot([]),
-    !environment.production ? StoreDevtoolsModule.instrument() : []
+
+
+    !environment.production ? StoreDevtoolsModule.instrument() : [],
+    StoreModule.forRoot(reducers, { metaReducers }),
+
   ],
   providers: [],
   bootstrap: [AppComponent]
