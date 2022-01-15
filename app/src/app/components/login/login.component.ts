11e15497b05577307b801400377e0c97d61d9c3b
Somers B Matthews <somersbmatthews@gmail.com>
2022-01-14 22:56:01 -0600

    completed dispatching action in login component with Store object

--- app/src/app/components/login/login.component.ts
+++ app/src/app/components/login/login.component.ts
@@ -1,6 +1,9 @@
+import { addUser } from './../store/user/user.actions';
 import { Component, OnInit } from '@angular/core';
 import { Store } from '@ngrx/store';
+import { timingSafeEqual } from 'crypto';
 import * as fromStore from '../store/user/user.reducer';
+import { stringify } from 'querystring';
 
 @Component({
   selector: 'app-login',
@@ -14,4 +17,8 @@ export class LoginComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  // method called when "register" button is pressed in UI
+  onSubmit(email: string, password: string) {
+    this.store.dispatch(addUser({email: email, password: password}));
+  }
 }
