53a4bc8ebe78472f54ffae50c328ad15095c435d
Somers B Matthews <somersbmatthews@gmail.com>
2022-01-14 19:44:38 -0600

    corrected auto header config

--- app/config/config.headerInterceptor.ts
+++ app/config/config.headerInterceptor.ts
@@ -9,7 +9,7 @@ import { Observable } from 'rxjs';
 export class AddHeaderInterceptor implements HttpInterceptor {
   intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
     // Clone the request to add the new header
-    const clonedRequest = req.clone({ headers: req.headers.append('Authorization', 'Bearer 123') });
+    const clonedRequest = req.clone({ headers: req.headers.append('Content-Type', 'application/json') });
 
     // Pass the cloned request instead of the original request to the next handle
     return next.handle(clonedRequest);
