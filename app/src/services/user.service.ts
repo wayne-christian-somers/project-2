37f9fa46b646b60c174e8f2b0c476fd4d7fdb5b5
Somers B Matthews <somersbmatthews@gmail.com>
2022-01-17 11:19:59 -0600

    changed user.service.ts

--- app/src/services/user.service.ts
+++ app/src/services/user.service.ts
@@ -6,11 +6,13 @@ import { User } from '../app/components/store/user/user.reducer'
 @Injectable({ providedIn: 'root' })
 export class UserService {
 
-  private baseUrl: string = "http://localhost:4200/"
+  private baseUrl: string = "http://localhost:5000/"
 
   constructor (private http: HttpClient) {}
 
   register(user : User) {
+    console.log("this is user data in register method");
+    console.log(user);
     return this.http.post(this.baseUrl + 'user/register', user)
   }
 
