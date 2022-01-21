cc783a8e8e0191ff8a558708254cd63da06aa26c
Somers B Matthews <somersbmatthews@gmail.com>
2022-01-20 18:23:08 -0600

    font awesome fixed

--- app/src/services/products.service.ts
+++ app/src/services/products.service.ts
@@ -1,13 +1,16 @@
 import { Injectable } from '@angular/core';
+import { nextTick } from 'process';
 
-var walmart = require('walmart')(environment);
+var walmart = require('walmart')(environment.WALMART_API_KEY);
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProductsService {
 
-  constructor() { }
+  constructor() {
+    console.log(environment.WALMART_API_KEY)
+   }
 
   // register(product : Product) {
 
